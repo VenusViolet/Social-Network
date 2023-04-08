7e79ee0599ffc6fb9c12d8ebd7afcc1394f6c49b
Venus Rodin <VenusViolet1234@gmail.com>
2023-04-07 20:51:44 -1000

    models, routes, and index.js

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+// create the User model using the userSchema
+const User = mongoose.model('User', userSchema);
+
+// export the User model
+module.exports = User;
