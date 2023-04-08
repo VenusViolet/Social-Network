01da6fbb72e16953c0e198384e19757f11ad9963
Venus Rodin <VenusViolet1234@gmail.com>
2023-04-08 10:10:45 -1000

    confused

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('../solid-broccoli/Develop/server/schema/schema');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = new ApolloServer({
   typeDefs,
---
+++ schemas/schemas.js
@@ -0,0 +1,11 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  // your GraphQL schema here
+`;
+
+const resolvers = {
+  // your resolvers here
+};
+
+module.exports = { typeDefs, resolvers };
